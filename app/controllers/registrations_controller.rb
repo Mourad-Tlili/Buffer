183259330bc86fbf6b095d038f114488370c39f6
mouradtlili <mourad.tlili@esprit.tn>
2022-04-17 01:49:10 +0100

    SignIn and Logout

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,17 @@ def new
 end
 
 def create
-    render plain: "Thanks!"
+    @user = User.new(user_params)
+    if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path, notice:"Successfully created user !"
+    else
+        render :new
+    end
 end
+
+private
+    def user_params
+    params.required(:user).permit(:email, :password,:password_confirmaton)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "sign_up", to: "registrations#new" 
   post "sign_up", to: "registrations#create"
+  delete "logout", to:"sessions#destroy"
 
+  get "sign_in", to:"sessions#new"
+  post "sign_in", to:"sessions#create"
   root to:"main#index"
 
 end
